beccdec7c3a7bb470c948e032255615d90b4a727
byiringiroaimefils <aimefils173@gmail.com>
2024-07-19 12:21:42 +0200

    New Chnge

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 // import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import View from './components/View'
 import Add from './components/Addcandidate'
 import Post from './components/Createpost'
 import Navigation from './components/Navigation'
 // import Header from './components/header'
-function App(){
-  return(
+function App() {
+  return (
     <>
-    <Navigation></Navigation>
-    <BrowserRouter>
-     <Routes>
-      <Route path='/' element ={<Navigation/>}/>
-      <Route path='/view'  element ={<View/>}/>
-      <Route path='/add'  element ={<Add/>}/>
-      <Route path='/post'  element ={<Post/>}/> 
-        
-      
-       
-    </Routes>
-    </BrowserRouter>
-    
+      {/* <Navigation> Here is  where issue locate  */}
+
+      <BrowserRouter>
+        <nav>
+          <Routes>
+            <Route path='/' element={<Navigation />} />
+            <Route path='/view' element={<View />} />
+            <Route path='/add' element={<Add />} />
+            <Route path='/post' element={<Post />} />
+          </Routes>
+        </nav>
+      </BrowserRouter>
+
+      {/* </Navigation> */}
     </>
 
-    
-    
+
+
 
   )
 }
 export default App
+
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,46 +1,44 @@
-// import React from "react"
+import React from "react"
 import Logo from '../assets/logo.jfif'
 import img from '../assets/house.jfif'
 // import Post from './Createpost' 
-import  Link  from "react-router-dom"
-function Navigation(){
-    return(
-    
+import {Link }from "react-router-dom"
+function Navigation() {
+    return (
         <>
-        <div className="container">
-            <div className="nav">
-            <span>ERMS</span>
-        <div className="img">
-            <img src={Logo} alt="" />
-        </div>
-        <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/add">AddCandidate</Link>
-        <Link to="/view">ViewCandidate</Link>
-        <Link to="/post">CreateNewPost</Link>
-        </div>
-            
-        </div>
-        </div>
-        <div className="body">
-            <div className="part1">
-            <img src={img} alt="" />
-            </div>
-            <div className="part2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non aperiam, itaque commodi unde vitae at dolorem repellendus provident officia recusandae quibusdam praesentium quisquam soluta? Accusantium ut voluptas eveniet nemo animi!
-           <div className="h">
-            <h2>Employment Recruitment Management System(ERMS)</h2>
-            <h3>Cafe Camilia</h3>
-            </div>
-            </div>
+            <div className="container">
+                <div className="nav">
+                    <span>ERMS</span>
+                    <div className="img">
+                        <img src={Logo} alt="" />
+                    </div>
+                    <div className="links">
+                        <Link to="/">Home</Link>
+                        <Link to="/Add">AddCandidate</Link>
+                        <Link to="/View">ViewCandidate</Link>
+                        <Link to="/Post">CreateNewPost</Link>
+                    </div>
 
-        </div>
-        <footer>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, distinctio ipsa. Placeat atque est iste ipsa beatae perferendis. Dolores ipsa voluptatibus eveniet ducimus, totam nisi id ullam molestias obcaecati beatae?</p>
-
-        </footer>
+                </div>
+            </div>
+            <div className="body">
+                <div className="part1">
+                    <img src={img} alt="" />
+                </div>
+                <div className="part2">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non aperiam, itaque commodi unde vitae at dolorem repellendus provident officia recusandae quibusdam praesentium quisquam soluta? Accusantium ut voluptas eveniet nemo animi!
+                    <div className="h">
+                        <h2>Employment Recruitment Management System(ERMS)</h2>
+                        <h3>Cafe Camilia</h3>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, distinctio ipsa. Placeat atque est iste ipsa beatae perferendis. Dolores ipsa voluptatibus eveniet ducimus, totam nisi id ullam molestias obcaecati beatae?</p>
+            </footer>
         </>
-        
     )
 }
 export default Navigation
+
+
